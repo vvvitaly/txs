a07c0ad6adc87935f534bc2f4c862f99e28ab390
vitaly.v <volkov.vitaly.v@gmail.com>
2019-08-20 01:27:21 +0300

    added base classes for parsing vmestecard

---
+++ src/Libs/Date/DateRange.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Libs\Date;
+
+use DateTimeImmutable;
+use Webmozart\Assert\Assert;
+
+/**
+ * Work with interval of dates. It's set by the begin and the end dates of this interval. Both dates might be set, or
+ * only one of them (begin or date, but not any of them).
+ */
+final class DateRange
+{
+    /**
+     * @var DateTimeImmutable|null
+     */
+    private $begin;
+
+    /**
+     * @var DateTimeImmutable|null
+     */
+    private $end;
+
+    /**
+     * @param DateTimeImmutable|null $begin
+     * @param DateTimeImmutable|null $end
+     */
+    public function __construct(?DateTimeImmutable $begin = null, ?DateTimeImmutable $end = null)
+    {
+        Assert::true($begin === null ^ $end === null || $begin !== null && $end !== null,
+            'At least one of the dates must be set');
+
+        if ($begin && $end) {
+            Assert::true($begin === null || $begin < $end, 'Begin date must be less than end date');
+        }
+
+        $this->begin = $begin;
+        $this->end = $end;
+    }
+
+    /**
+     * Check if the given date is in the specified range.
+     *
+     * @param DateTimeImmutable $testingDate
+     *
+     * @return bool
+     */
+    public function inRange(DateTimeImmutable $testingDate): bool
+    {
+        return
+            ($this->begin === null || $testingDate >= $this->begin) &&
+            ($this->end === null || $testingDate <= $this->end);
+    }
+}
---
+++ tests/Libs/Date/DateRangeTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/** @noinspection PhpMissingDocCommentInspection */
+
+declare(strict_types=1);
+
+namespace tests\Libs\Date;
+
+use App\Libs\Date\DateRange;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+final class DateRangeTest extends TestCase
+{
+    /**
+     * @dataProvider providerInRange
+     *
+     * @param DateTimeImmutable $testingDate
+     * @param DateTimeImmutable|null $begin
+     * @param DateTimeImmutable|null $end
+     * @param bool $expectedIsIn
+     */
+    public function testInRange(
+        DateTimeImmutable $testingDate,
+        ?DateTimeImmutable $begin,
+        ?DateTimeImmutable $end,
+        bool $expectedIsIn
+    ): void {
+        $range = new DateRange($begin, $end);
+        $this->assertEquals($expectedIsIn, $range->inRange($testingDate));
+    }
+
+    public function providerInRange(): array
+    {
+        $dt = static function (string $date) {
+            return new DateTimeImmutable($date);
+        };
+
+        return [
+            'in, two dates' => [
+                $dt('2019-08-02 15:21:49'),
+                $dt('2019-08-01 23:12:22'),
+                $dt('2019-08-03 13:23:47'),
+                true,
+            ],
+            'greater, two dates' => [
+                $dt('2019-08-03 13:23:48'),
+                $dt('2019-08-01 23:12:22'),
+                $dt('2019-08-03 13:23:47'),
+                false,
+            ],
+            'less, two dates' => [
+                $dt('2019-08-01 13:23:46'),
+                $dt('2019-08-01 23:12:22'),
+                $dt('2019-08-03 13:23:47'),
+                false,
+            ],
+            'in, only start' => [
+                $dt('2123-08-02 15:21:49'),
+                $dt('2019-08-01 23:12:22'),
+                null,
+                true,
+            ],
+            'less, only start' => [
+                $dt('2019-08-01 15:21:49'),
+                $dt('2019-08-01 23:12:22'),
+                null,
+                false,
+            ],
+            'in, only end' => [
+                $dt('1627-08-02 15:21:49'),
+                null,
+                $dt('2019-08-01 23:12:22'),
+                true,
+            ],
+            'greater, only end' => [
+                $dt('2182-08-02 15:21:49'),
+                null,
+                $dt('2019-08-01 23:12:22'),
+                false,
+            ],
+        ];
+    }
+}
