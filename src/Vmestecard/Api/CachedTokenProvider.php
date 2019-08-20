3d5b7ad70f59c1464fca0591aa7f06437345a9b6
vitaly.v <volkov.vitaly.v@gmail.com>
2019-08-21 00:46:55 +0300

    added token provider with cache

---
+++ src/Vmestecard/Api/CachedTokenProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Vmestecard\Api;
+
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * Cache access token. This token provider tries to read access token from cache with the timestamp when token was
+ * cached. If token is found and is still valid it returns without calling wrapped provider. Otherwise the inner
+ * provider will be called.
+ */
+final class CachedTokenProvider implements TokenProviderInterface
+{
+    /**
+     * @var TokenProviderInterface
+     */
+    private $inner;
+
+    /**
+     * @var CacheInterface
+     */
+    private $cache;
+
+    /**
+     * @var string
+     */
+    private $cachePrefix;
+
+    /**
+     * @param TokenProviderInterface $inner
+     * @param CacheInterface $cache
+     * @param string $cachePrefix
+     */
+    public function __construct(TokenProviderInterface $inner, CacheInterface $cache, string $cachePrefix)
+    {
+        $this->inner = $inner;
+        $this->cache = $cache;
+        $this->cachePrefix = $cachePrefix;
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public function getToken(): ApiToken
+    {
+        $key = "{$this->cachePrefix}access-token";
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $tokenData = $this->cache->get($key);
+
+        $token = null;
+        if ($tokenData !== null) {
+            [$cachedTimestamp, $tokenValue, $lifetime] = $tokenData;
+            $token = new ApiToken($tokenValue, $lifetime - (time() - $cachedTimestamp));
+        }
+
+        if (!$token || !$token->isValid()) {
+            $token = $this->inner->getToken();
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $this->cache->set($key, [time(), $token->getToken(), $token->getLifetime()], $token->getLifetime());
+        }
+
+        return $token;
+    }
+}
---
+++ tests/Vmestecard/Api/CachedTokenProviderTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\Vmestecard\Api;
+
+use App\Vmestecard\Api\ApiToken;
+use App\Vmestecard\Api\CachedTokenProvider;
+use App\Vmestecard\Api\TokenProviderInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\SimpleCache\CacheInterface;
+
+/** @noinspection PhpMissingDocCommentInspection */
+
+final class CachedTokenProviderTest extends TestCase
+{
+    public function testGetTokenWhenNoCache(): void
+    {
+        $token = new ApiToken('test', 1);
+
+        $inner = $this->createMock(TokenProviderInterface::class);
+        $inner->expects($this->once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        $cache = $this->createMock(CacheInterface::class);
+        $cache->expects($this->once())
+            ->method('get')
+            ->with('txs.access-token')
+            ->willReturn(null);
+
+        $tokenProvider = new CachedTokenProvider($inner, $cache, 'txs.');
+        $actual = $tokenProvider->getToken();
+
+        $this->assertSame($token, $actual);
+    }
+
+    public function testGetTokenFromCache(): void
+    {
+        $inner = $this->createMock(TokenProviderInterface::class);
+        $inner->expects($this->never())
+            ->method('getToken');
+
+        $cache = $this->createMock(CacheInterface::class);
+        $cache->expects($this->once())
+            ->method('get')
+            ->with('txs.access-token')
+            ->willReturn([strtotime('-1 hour'), 'some token', 4000]);
+
+        $tokenProvider = new CachedTokenProvider($inner, $cache, 'txs.');
+        $actual = $tokenProvider->getToken();
+
+        $this->assertEquals('some token', $actual->getToken());
+        $this->assertEquals(400, $actual->getLifetime());
+    }
+
+    public function testGetTokenFromCacheButExpired(): void
+    {
+        $token = new ApiToken('test', 1);
+
+        $inner = $this->createMock(TokenProviderInterface::class);
+        $inner->expects($this->once())
+            ->method('getToken')
+            ->willReturn($token);
+
+        $cache = $this->createMock(CacheInterface::class);
+        $cache->expects($this->once())
+            ->method('get')
+            ->with('txs.access-token')
+            ->willReturn([strtotime('-1 hour'), 'some token', 3600]);
+
+        $tokenProvider = new CachedTokenProvider($inner, $cache, 'txs.');
+        $actual = $tokenProvider->getToken();
+
+        $this->assertSame($token, $actual);
+    }
+}
