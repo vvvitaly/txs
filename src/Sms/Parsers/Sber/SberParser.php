5a5095027c961fc66da21234da97027e075ea92d
vitaly.v <volkov.vitaly.v@gmail.com>
2019-10-03 10:57:49 +0300

    removed validation trait and used validation decorator instead

--- src/Sms/Parsers/Sber/SberParser.php
+++ src/Sms/Parsers/Sber/SberParser.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace vvvitaly\txs\Sms\Parsers\Sber;
 
-use ArrayObject;
 use vvvitaly\txs\Core\Bills\Bill;
 use vvvitaly\txs\Sms\Message;
 use vvvitaly\txs\Sms\Parsers\CompositeMessageParser;
 use vvvitaly\txs\Sms\Parsers\MessageParserInterface;
-use vvvitaly\txs\Sms\Parsers\Sber\ComplexTransfer\ConfirmationSmsParser;
-use vvvitaly\txs\Sms\Parsers\Sber\ComplexTransfer\PinSmsParser;
-use vvvitaly\txs\Sms\Parsers\Sber\ComplexTransfer\SberComplexTransfer;
-use vvvitaly\txs\Sms\Parsers\Sber\PinParser\ArrayStorage;
 
 /**
  * Composite parser for all other parsers
@@ -28,18 +23,20 @@ final class SberParser implements MessageParserInterface
      */
     public function __construct()
     {
-        $this->parser = new CompositeMessageParser(
+        $parser = new CompositeMessageParser(
             new SberPayment(),
             new SberPurchase(),
             new SberTransfer(),
-            new SberComplexTransfer(
-                new ArrayStorage(new ArrayObject()),
-                new PinSmsParser(),
-                new ConfirmationSmsParser()
-            ),
+//            new SberComplexTransfer(
+//                new ArrayStorage(new ArrayObject()),
+//                new PinSmsParser(),
+//                new ConfirmationSmsParser()
+//            ),
             new SberWithdrawal(),
             new SberRefill()
         );
+
+        $this->parser = new SberValidationDecorator($parser);
     }
 
     /**
---
+++ tests/Sms/Parsers/Sber/SberValidationDecoratorTest.php
@@ -0,0 +1,45 @@
+<?php /** @noinspection PhpMissingDocCommentInspection */
+
+declare(strict_types=1);
+
+namespace tests\Sms\Parsers\Sber;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use vvvitaly\txs\Sms\Message;
+use vvvitaly\txs\Sms\Parsers\MessageParserInterface;
+use vvvitaly\txs\Sms\Parsers\Sber\SberValidationDecorator;
+
+final class SberValidationDecoratorTest extends TestCase
+{
+    /**
+     * @param \vvvitaly\txs\Sms\Message $sms
+     * @param $expectedValid
+     *
+     * @dataProvider providerParse
+     */
+    public function testParse(Message $sms, $expectedValid): void
+    {
+        $inner = $this->createMock(MessageParserInterface::class);
+
+        if ($expectedValid) {
+            $inner->expects($this->once())
+                ->method('parse')
+                ->with($this->identicalTo($sms));
+        } else {
+            $inner->expects($this->never())
+                ->method('parse');
+        }
+
+        $decorator = new SberValidationDecorator($inner);
+        $decorator->parse($sms);
+    }
+
+    public function providerParse(): array
+    {
+        return [
+            'from 900' => [new Message('900', new DateTimeImmutable(), 'some'), true],
+            'wrong address' => [new Message('9000', new DateTimeImmutable(), 'some'), false],
+        ];
+    }
+}
